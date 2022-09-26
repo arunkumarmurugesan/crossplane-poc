b0548633004d4feb0311c7c710231e7f6e849272
arunkumar.murugesan <akumar28@vmware.com>
2022-09-26 19:55:50 +0530

    Adding tf deployment with compositions

--- terraform-infra/tf/crossplane-composition/tf/main.tf
+++ terraform-infra/tf/crossplane-composition/tf/main.tf
@@ -1,20 +1,19 @@
 resource "aws_s3_bucket" "paas-cp" {
-    bucket = var.bucket
-    force_destroy = var.forcedestroy
-    tags = {
-        Name        = var.bucket
-        Environment = var.env
+bucket = var.Bucket
+force_destroy = var.Forcedestroy
+tags = {
+    Name        = var.Bucket
+    Environment = var.env
     }
 }
-
 resource "aws_s3_bucket_acl" "acl-test" {
     bucket = aws_s3_bucket.paas-cp.id
     acl    = var.acl
 }
 
 resource "aws_s3_bucket_versioning" "versioning_test" {
-    bucket = aws_s3_bucket.paas-cp.id
-    versioning_configuration {
-        status = var.versioningconfiguration
-    }
+  bucket = aws_s3_bucket.paas-cp.id
+  versioning_configuration {
+    status = var.VersioningConfiguration
+  }
 }
